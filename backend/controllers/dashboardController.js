1f895b0b9a49f1fd8ece8c0228a4f1fc92b6f548
Aayush Kansal <aayushkansal79@gmail.com>
2025-07-31 18:33:32 +0530

    bug-6

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -1,27 +1,33 @@
+import Purchase from "../models/Purchase.js";
 import Company from "../models/Company.js";
 import Product from "../models/Product.js";
-import Purchase from "../models/Purchase.js";
+import Assignment from "../models/Assignment.js";
 import Store from "../models/Store.js";
 import Bill from "../models/Bill.js";
+import Customer from "../models/Customer.js";
 import ProductRequest from "../models/ProductRequest.js";
 
 export const getDashboardCounts = async (req, res) => {
     try {
-        const [companyCount, productCount, purchaseCount, storeCount, billCount, reqCount] = await Promise.all([
+        const [purchaseCount, companyCount, productCount , assignCount, storeCount, billCount, customerCount, reqCount] = await Promise.all([
+            Purchase.countDocuments(),
             Company.countDocuments(),
             Product.countDocuments(),
-            Purchase.countDocuments(),
+            Assignment.countDocuments(),
             Store.countDocuments({type:"store"}),
             Bill.countDocuments(),
+            Customer.countDocuments(),
             ProductRequest.countDocuments(),
         ]);
 
         res.status(200).json({
+            purchases: purchaseCount,
             companies: companyCount,
             products: productCount,
-            purchases: purchaseCount,
+            assignments: assignCount,
             stores: storeCount,
             bills: billCount,
+            customers: customerCount,
             req: reqCount,
         });
     } catch (err) {
--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -19,14 +19,17 @@ const Dashboard = ({ url }) => {
     document.title = "Dashboard | Ajjawam";
   }, []);
 
-  const token = sessionStorage.getItem("token") || localStorage.getItem("token");
+  const token =
+    sessionStorage.getItem("token") || localStorage.getItem("token");
 
   const [counts, setCounts] = useState({
     companies: 0,
     products: 0,
+    assignments: 0,
     purchases: 0,
     stores: 0,
     bills: 0,
+    customers: 0,
     req: 0,
   });
 
@@ -125,6 +128,23 @@ const Dashboard = ({ url }) => {
           </div>
         </Link>
 
+        <Link to="/assignments" className="card text-bg-light mb-3">
+          <div className="card-header">Assignments</div>
+          <div className="card-body">
+            <p className="card-text orange">{counts.assignments}</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="70px"
+              viewBox="0 -960 960 960"
+              width="50px"
+              fill="#000000"
+              className="orange"
+            >
+              <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h168q13-36 43.5-58t68.5-22q38 0 68.5 22t43.5 58h168q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm80-80h280v-80H280v80Zm0-160h400v-80H280v80Zm0-160h400v-80H280v80Zm200-190q13 0 21.5-8.5T510-820q0-13-8.5-21.5T480-850q-13 0-21.5 8.5T450-820q0 13 8.5 21.5T480-790ZM200-200v-560 560Z" />
+            </svg>
+          </div>
+        </Link>
+
         <Link to="/all-stores" className="card text-bg-light mb-3">
           <div className="card-header">Stores</div>
           <div className="card-body">
@@ -155,6 +175,23 @@ const Dashboard = ({ url }) => {
               className="red"
             >
               <path d="M120-80v-800l60 60 60-60 60 60 60-60 60 60 60-60 60 60 60-60 60 60 60-60 60 60 60-60v800l-60-60-60 60-60-60-60 60-60-60-60 60-60-60-60 60-60-60-60 60-60-60-60 60Zm120-200h480v-80H240v80Zm0-160h480v-80H240v80Zm0-160h480v-80H240v80Zm-40 404h560v-568H200v568Zm0-568v568-568Z" />
+            </svg>
+          </div>
+        </Link>
+
+        <Link to="/all-customer" className="card text-bg-light mb-3">
+          <div className="card-header">Customers</div>
+          <div className="card-body">
+            <p className="card-text red">{counts.customers}</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="70px"
+              viewBox="0 -960 960 960"
+              width="50px"
+              fill="#000000"
+              className="red"
+            >
+              <path d="M40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm720 0v-120q0-44-24.5-84.5T666-434q51 6 96 20.5t84 35.5q36 20 55 44.5t19 53.5v120H760ZM360-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47Zm400-160q0 66-47 113t-113 47q-11 0-28-2.5t-28-5.5q27-32 41.5-71t14.5-81q0-42-14.5-81T544-792q14-5 28-6.5t28-1.5q66 0 113 47t47 113ZM120-240h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T440-640q0-33-23.5-56.5T360-720q-33 0-56.5 23.5T280-640q0 33 23.5 56.5T360-560Zm0 320Zm0-400Z" />
             </svg>
           </div>
         </Link>
