ea387b6ce2bf3da01cfd4155a8b2aae6a25d2d3b
Aayush Kansal <aayushkansal79@gmail.com>
2025-08-02 16:48:18 +0530

    page

--- frontend/src/components/Pagination/Pagination.jsx
+++ frontend/src/components/Pagination/Pagination.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import './Pagination.css'
+import React from "react";
+import "./Pagination.css";
 
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
-    const getPageNumbers = () => {
+  const getPageNumbers = () => {
     const maxVisible = 5;
     const half = Math.floor(maxVisible / 2);
 
@@ -10,33 +10,51 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
     let end = Math.min(totalPages, start + maxVisible - 1);
 
     if (end - start < maxVisible - 1) {
-        start = Math.max(1, end - maxVisible + 1);
+      start = Math.max(1, end - maxVisible + 1);
     }
-    
+
     const pages = [];
     for (let i = start; i <= end; i++) {
-        pages.push(i);
+      pages.push(i);
     }
-    
+
     return pages;
-};
+  };
 
-const pages = getPageNumbers();
+  const pages = getPageNumbers();
 
-return (
+  return (
     <div className="pagination">
       <button
         onClick={() => onPageChange(1)}
         disabled={currentPage === 1 || totalPages === 0}
+      >
+        {/* First */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="20px"
+          viewBox="0 -960 960 960"
+          width="20px"
+          fill="#000000"
         >
-        First
+          <path d="M440-240 200-480l240-240 56 56-183 184 183 184-56 56Zm264 0L464-480l240-240 56 56-183 184 183 184-56 56Z" />
+        </svg>
       </button>
 
       <button
         onClick={() => onPageChange(currentPage - 1)}
         disabled={currentPage === 1 || totalPages === 0}
+      >
+        {/* Prev */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="20px"
+          viewBox="0 -960 960 960"
+          width="20px"
+          fill="#000000"
         >
-        Prev
+          <path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z" />
+        </svg>
       </button>
 
       {pages.map((page) => (
@@ -44,7 +62,7 @@ return (
           key={page}
           onClick={() => onPageChange(page)}
           className={page === currentPage ? "active" : ""}
-          >
+        >
           {page}
         </button>
       ))}
@@ -53,17 +71,35 @@ return (
         onClick={() => onPageChange(currentPage + 1)}
         disabled={currentPage === totalPages || totalPages === 0}
       >
-        Next
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="20px"
+          viewBox="0 -960 960 960"
+          width="20px"
+          fill="#000000"
+        >
+          <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
+        </svg>
+        {/* Next */}
       </button>
 
       <button
         onClick={() => onPageChange(totalPages)}
         disabled={currentPage === totalPages || totalPages === 0}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="20px"
+          viewBox="0 -960 960 960"
+          width="20px"
+          fill="#000000"
         >
-        Last
+          <path d="M383-480 200-664l56-56 240 240-240 240-56-56 183-184Zm264 0L464-664l56-56 240 240-240 240-56-56 183-184Z" />
+        </svg>
+        {/* Last */}
       </button>
     </div>
   );
 };
 
-export default Pagination
+export default Pagination;
